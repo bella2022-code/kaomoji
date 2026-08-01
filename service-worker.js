56bca778efc1c886f99561ccd3fd656354f0724d
bella2022-code <bellachang2022@gmail.com>
2026-08-01 17:16:03 +0800

    Refresh symbol pack cache

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE = 'kaomoji-v15';
-const ASSETS = ['./', './index.html', './styles.css?v=20260801-stable-picks', './script.js?v=20260801-stable-picks', './manifest.webmanifest', './favicon.svg'];
+const CACHE = 'kaomoji-v17';
+const ASSETS = ['./', './index.html', './styles.css?v=20260801-apple-safe', './script.js?v=20260801-apple-safe', './manifest.webmanifest', './favicon.svg'];
 
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
