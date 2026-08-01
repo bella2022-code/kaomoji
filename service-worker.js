87e9bc164deaeb5aed1cce5457ec29ad45f9121f
bella2022-code <bellachang2022@gmail.com>
2026-08-01 16:54:56 +0800

    Refresh angry kaomoji cache

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE = 'kaomoji-v10';
-const ASSETS = ['./', './index.html', './styles.css?v=20260801-symbol-fix', './script.js?v=20260801-symbol-fix', './manifest.webmanifest', './favicon.svg'];
+const CACHE = 'kaomoji-v11';
+const ASSETS = ['./', './index.html', './styles.css?v=20260801-angry-face', './script.js?v=20260801-angry-face', './manifest.webmanifest', './favicon.svg'];
 
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
